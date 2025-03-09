93e9e47c75bbf8229a36fc3878a398b8ae312d9a
hady mohamed <hady912012@gmail.com>
2025-03-10 00:35:24 +0200

    update front

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let menu = document.querySelector(".menu");
 let lang = document.querySelector(".lang");
 let langText = document.querySelector(".lang-text");
 let elementToToggle = document.getElementById("default-sidebar");
+let collapse = document.getElementById("collapse");
+let news = document.getElementById("news");
 
 closeSide.addEventListener("click", () => {
   elementToToggle.classList.toggle("-translate-x-full");
@@ -18,6 +20,12 @@ lang.addEventListener("click", () => {
   const text = langText.innerHTML;
   changeHtmlAttributes(text === "Ar" ? "en" : "ar");
   langText.innerHTML = text === "Ar" ? "EN" : "Ar";
+});
+
+collapse.addEventListener("click", () => {
+  news.classList.toggle("-bottom-[50px]");
+  news.classList.toggle("bottom-0");
+  collapse.querySelector(".svg").classList.toggle("rotate-180");
 });
 
 function changeHtmlAttributes(lang) {
